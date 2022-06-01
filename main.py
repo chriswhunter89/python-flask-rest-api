9439411ea02a832d7fc5698ff4ae7d5aa086effd
Chris Hunter <chriswhunter89@gmail.com>
2022-06-01 21:56:49 +0100

    App initiated and testing complete.

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful  import Api, Resource
 
 app = Flask(__name__) #always added when creating flask app
 api = Api(app) # this statment says we will wrap the app in an API and initialises the restful API
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"data": "Hello World"} #objects returned from an api must be json serialisable (dictionaries)
+
+api.add_resource(HelloWorld, "/helloworld")
+
+# starts the server and flask application
+if __name__ == "__main__":
+    app.run(debug=True)
