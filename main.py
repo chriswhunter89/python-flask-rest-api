44ffafc14493885bdebdeef453529cd187345c6b
Chris Hunter <chriswhunter89@gmail.com>
2022-06-02 20:01:57 +0100

    Dependencies added, virtual environment setup and tests on user dictionaries.

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from flask_restful  import Api, Resource
 app = Flask(__name__) #always added when creating flask app
 api = Api(app) # this statment says we will wrap the app in an API and initialises the restful API
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"data": "Hello World"} #objects returned from an api must be json serialisable (dictionaries)
+names = {"chris": {"age": 32, "gender": "male"}, "presto": {"age": 72, "gender": "male"}}
 
-api.add_resource(HelloWorld, "/helloworld")
+class HelloWorld(Resource):
+    def get(self, name):
+        return names[name] #objects returned from an api must be json serialisable (dictionaries)
+
+    def post(self):
+        return {"data": "Posted"}
+
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
 # starts the server and flask application
 if __name__ == "__main__":
