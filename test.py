1fbad3abf80e335962bef6ce6740bfab0763313a
Chris Hunter <chriswhunter89@gmail.com>
2022-06-03 10:03:57 +0100

    Arg parser, status code and request validation implemented.

--- test.py
+++ test.py
@@ -2,5 +2,8 @@ import requests #module that allows us to make requests(post, get, patch etc.)
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld/presto")
+# response = requests.put(BASE + "video/1", {"likes": 10, "name": "chris", "views": 100000})
+# print(response.json())
+# input()
+response = requests.get(BASE + "video/2")
 print(response.json())
