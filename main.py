6cdca6e733d58dff9fc96e357c071d225919ba2a
Chris Hunter <chriswhunter89@gmail.com>
2022-06-03 12:02:15 +0100

    Database created, configured, and edge cases added.

--- main.py
+++ main.py
@@ -1,30 +1,60 @@
 from flask import Flask
-from flask_restful  import Api, Resource, reqparse
-from flask import abort
+from flask_restful  import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) #always added when creating flask app
 api = Api(app) # this statment says we will wrap the app in an API and initialises the restful API
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name={name}, views={views}, likes={likes})" #repr = representation. just allows you to print and inspect an onject
+
+# db.create_all() *** commented out after we ran it once as we dont want to delete our data
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
 video_put_args.add_argument("views", type=int, help="Views of the video is required", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes of the video is required", required=True)
 
-videos = {}
-
-def abort_if_video_id_doesnt_exist(video_id):
-    if video_id not in videos:
-        abort(404, "Video id is not valid . . . ")
+resource_fields = {
+    "id": fields.Integer,
+    "name": fields.String,
+    "views": fields.Integer,
+    "likes": fields.Integer
+}
 
 class Video(Resource):
+    @marshal_with(resource_fields) #serialises resource fields into json format that can be returned
     def get(self, video_id):
-        abort_if_video_id_doesnt_exist(video_id)
-        return videos[video_id] # objects returned from an api must be json serialisable (dictionaries)
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Could not find video with that ID")
+        return result
 
+    @marshal_with(resource_fields)
     def put(self, video_id):
         args = video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201 #returns created video and status code
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message="Video id taken")
+
+        video = VideoModel(id=video_id, name=args["name"], views=args["views"], likes=args["likes"])
+        db.session.add(video) #temporarily adds to db
+        db.session.commit() #persists to db
+        return video, 201 #returns created video and status code
+
+    def delete(self, video_id):
+        del videos[video_id]
+        return "", 204
+
+
 api.add_resource(Video, "/video/<int:video_id>")
 
 # starts the server and flask application
--- test.py
+++ test.py
@@ -2,8 +2,13 @@ import requests #module that allows us to make requests(post, get, patch etc.)
 
 BASE = "http://127.0.0.1:5000/"
 
-# response = requests.put(BASE + "video/1", {"likes": 10, "name": "chris", "views": 100000})
-# print(response.json())
-# input()
-response = requests.get(BASE + "video/2")
+data = [{"name": "In the name of the father", "likes": 324,  "views": 894000}, {"name": "Saving Private Ryan", "likes": 256, "views": 423500}, {"name": "Garfield", "likes": 8,  "views": 121}]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+input()
+
+response = requests.get(BASE + "video/6")
 print(response.json())
